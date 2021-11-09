cdf9d64f6f0c097ca4521e0de768efac379adcc6
maithu0809 <minhthu08092019@gmail.com>
2021-11-09 09:56:42 +0700

    screen 1

---
+++ Screen1/app/src/main/java/com/example/btlandroid_nhom10_appbanmypham/Screen2.java
@@ -0,0 +1,18 @@
+package com.example.btlandroid_nhom10_appbanmypham;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class Screen2 extends AppCompatActivity {
+    private ListView lv;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen2);
+    }
+}
